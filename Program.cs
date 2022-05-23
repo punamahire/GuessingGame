3555d2550f1103b56cf09345599adc7607a46135
Punam Ahire <punambahire@gmail.com>
2022-05-23 16:10:43 -0500

    phase 8

--- Program.cs
+++ Program.cs
@@ -6,18 +6,41 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            // Phase 1
-            for (int i = 0; i < 4; i++)
+            Console.Write("Enter a difficulty level you want to try (Easy/Medium/Hard)? ");
+            string diffLevel = Console.ReadLine();
+
+            int counter = 0;
+            bool continueGame = false;
+            if (diffLevel == "easy" || diffLevel == "Easy")
             {
-                Console.Write("Guess the secret number: ");
+                counter = 8;
+            }
+            else if (diffLevel == "medium" || diffLevel == "Medium")
+            {
+                counter = 6;
+            }
+            else if (diffLevel == "hard" || diffLevel == "Hard")
+            {
+                counter = 4;
+            }
+            else
+            {
+                //Console.Write("Please enter a correct difficulty level: ");
+                continueGame = true;
+            }
+
+            int secNumber = GenerateRandomNum();
+
+            for (int i = 0; i < counter; i++)
+            {
+                Console.Write("Guess the secret number (1-100): ");
                 string input = Console.ReadLine();
                 int num = int.Parse(input);
                 //Console.WriteLine(num);
 
                 // Phase 2
                 // int secNumber = 42;
-                Random r = new Random();
-                int secNumber = r.Next(1, 100);
+
                 if (num == secNumber)
                 {
                     Console.WriteLine("You guessed it right! Way to go...");
@@ -26,13 +49,56 @@ namespace GuessingGame
                 else
                 {
                     Console.WriteLine($"Your guess ({num})");
-                    Console.WriteLine($"You have {3 - i} more chances to guess.");
-                }
+                    Console.WriteLine($"You have {(counter - 1) - i} more chances to guess.");
 
+                    if (num > secNumber && num - secNumber >= 50)
+                    {
+                        Console.WriteLine("Your guess is too high.");
+                    }
+                    else if (num < secNumber && secNumber - num >= 50)
+                    {
+                        Console.WriteLine("Your guess is too low.");
+                    }
+                }
             }
 
-            // Phase 3 - wrapped code within for loop
+            while (continueGame)
+            {
+                Console.Write("Guess the secret number (1-100): ");
+                string input = Console.ReadLine();
+                int num = int.Parse(input);
+                //Console.WriteLine(num);
 
+                // Phase 2
+                // int secNumber = 42;
+
+                if (num == secNumber)
+                {
+                    Console.WriteLine("You guessed it right! Way to go...");
+                    continueGame = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Your guess ({num})");
+
+                    if (num > secNumber && num - secNumber >= 50)
+                    {
+                        Console.WriteLine("Your guess is too high.");
+                    }
+                    else if (num < secNumber && secNumber - num >= 50)
+                    {
+                        Console.WriteLine("Your guess is too low.");
+                    }
+                }
+            }
+        }
+        public static int GenerateRandomNum()
+        {
+            Random r = new Random();
+            int secNum = r.Next(1, 101);
+            Console.WriteLine($"Secret Number: {secNum}");
+            return secNum;
         }
     }
 }
